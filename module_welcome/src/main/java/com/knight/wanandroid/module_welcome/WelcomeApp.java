f20fa56e4b0a7acba62e9163b2dc25fd72ab42c0
luguian <luga@chetong.net>
2021-09-09 20:52:40 +0800

    【gradle模块化】gradle模块化完善

--- module_welcome/src/main/java/com/knight/wanandroid/module_welcome/WelcomeApp.java
+++ module_welcome/src/main/java/com/knight/wanandroid/module_welcome/WelcomeApp.java
@@ -3,6 +3,8 @@ package com.knight.wanandroid.module_welcome;
 import android.app.Application;
 
 import com.knight.wanandroid.library_base.BaseApp;
+import com.knight.wanandroid.library_base.initconfig.ModuleConfig;
+import com.knight.wanandroid.library_util.LogUtils;
 
 /**
  * @author created by knight
@@ -11,6 +13,16 @@ import com.knight.wanandroid.library_base.BaseApp;
  * @descript:
  */
 public final class WelcomeApp extends BaseApp {
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        if ("true".equals(BuildConfig.isAloneApp)) {
+            LogUtils.d("123456");
+            ModuleConfig.getInstance().initBefore(this);
+            ModuleConfig.getInstance().initModuleAfter(this);
+        }
+    }
+
     @Override
     public void initModuleApp(Application application) {
 
